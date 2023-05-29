7485be038f6f265c1669a506f69b756821e7dd50
Awnish Kumar Singh <51233811+Dmsingh@users.noreply.github.com>
2023-05-29 18:28:47 +0530

    Update Header.ts
    Removing local host

--- src/utility/Header.ts
+++ src/utility/Header.ts
@@ -17,10 +17,8 @@ export const getHeader = async (body: any) => {
   const UKID: any = process.env.UNIQUEKEYID;
   const algorithm: any = process.env.ALGORITHM;
   const header = {
-    "host": process.env.LOCALHOST,
     "accept": "application/json",
     "authorization": `Signature keyId="${subscriber_id}|${UKID}|${algorithm}",algorithm="ed25519",created=${created},expires=${expires},headers="(created) (expires) digest",signature="${digest_base64}"`,
-    'user-agent': "ReactorNetty/1.0.19",
     'content-type': 'application/json',
   };
   log.debug("Looking for header  before return in utility/header.ts -> ","getHeader()",{header});
